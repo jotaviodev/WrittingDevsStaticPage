4a347bdb87a73bb161f9d1757d8457e7c1c9502b
jotaviodev <otaviomatosxd3@gmail.com>
2024-04-04 17:25:25 -0300

    Transfering CSS functions for JS

---
+++ script.js
@@ -0,0 +1,12 @@
+function zoomListing(obj){
+    obj.style.color="whitesmoke"
+    obj.style.transform="scale(1.1)"
+    obj.style.backgroundPosition="-60px"
+    obj.style.textShadow="0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)"
+}
+function returnZoom(obj){
+    obj.style.color="#909090"
+    obj.style.transform="scale(1)"
+    obj.style.backgroundPosition="-0"
+    obj.style.textShadow="0"
+}
